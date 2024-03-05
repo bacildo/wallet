c2804472f6c95c6e9fedbf54a93cf9d1b06cee60
bacildo <dbacildodasilva@gmail.com>
2024-03-05 12:05:44 -0300

    feat: components Login and Home

--- spa-wallet/src/App.jsx
+++ spa-wallet/src/App.jsx
@@ -1,6 +1,11 @@
+import Login from "./pages/Login";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <Login />
+    </main>
+  );
 }
 
 export default App;
---
+++ spa-wallet/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Login from "./Login";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center bg-zinc-900 rounded p-8 w-[60rem] h-[35rem] text 2xl">
+      <Login />
+    </main>
+  );
+}
